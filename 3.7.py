b9ca2f0435f9779deafc12a217906544b1c72b3a
deepu0428 <adeepikareddy31@gmail.com>
2025-06-04 00:22:48 +0530

    Add files via upload

---
+++ 3.7.py
@@ -0,0 +1,12 @@
+list=[10,20,30,"milky",true,0,(10+20j),false,1,true]
+print(list)
+x=list.copy()
+print(x)
+list.pop(3)
+print(list)
+del.list[2]
+print(list)
+list.reverse()
+print(list)
+list.clear()
+print(list)
